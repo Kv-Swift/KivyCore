7959a540d6f5ef89a77be1ae9d79da008738fd99
kivyswiftlink <kivyswiftlink@gmail.com>
2025-04-25 19:27:57 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/8bdf080f902ad86495c21a91aded8219a466f07f

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/KivySwiftLink/SDL2Core", .upToNextMajor(from: "311.0.0")),
-        //.package(url: "https://github.com/KivySwiftLink/PythonSwiftLink", .upToNextMajor(from: "311.1.0")),
         .package(url: "https://github.com/KivySwiftLink/PySwiftKit", .upToNextMajor(from: "311.0.0")),
         .package(url: "https://github.com/KivySwiftLink/PythonCore", .upToNextMajor(from: "311.0.0")),
         .package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", .upToNextMajor(from: "0.0.0"))
@@ -37,8 +36,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libkivy",
-            url: "https://github.com/kivyswiftlink/KivyCore/releases/download/311.1.0/libkivy.zip",
-            checksum: "8569e596e5167f93573e7a1a2a7251f1c7c5d68e7d72517d46bb52ec61692761"
+            url: "https://github.com/kivyswiftlink/KivyCore/releases/download/311.1.1/libkivy.zip",
+            checksum: "49447aa23ad6eedf8bd59f777041d8305c64d304aa6ae33cf7862ca1580eefff"
         )
     ]
 )
