9203a04eebd61b2a1d9245b2b56eebe011c65bcc
kivyswiftlink <kivyswiftlink@gmail.com>
2025-05-18 10:20:00 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/b61ad35329bdc55eb31072d580a6e1e20dd4886f

--- Package.swift
+++ Package.swift
@@ -2,35 +2,6 @@
 
 import PackageDescription
 
-
-let kivy = true
-let local = false
-
-let pykit_package: Package.Dependency = if kivy {
-    .package(url: "https://github.com/KivySwiftLink/PySwiftKit", from: .init(311, 0, 0))
-} else {
-    if local {
-        .package(path: "/Users/codebuilder/Documents/GitHub/PySwiftKit")
-    } else {
-        .package(url: "https://github.com/PythonSwiftLink/PySwiftKit", from: .init(311, 0, 0))
-    }
-}
-
-let pykit: Target.Dependency = .product(name: "SwiftonizeModules", package: "PySwiftKit")
-
-
-let sdl_package: Package.Dependency = if local {
-    .package(path: "../SDL2Core")
-} else {
-    .package(url: "https://github.com/KivySwiftLink/SDL2Core", .upToNextMajor(from: "311.0.0"))
-}
-
-let psw: Package.Dependency = if local {
-    .package(path: "/Volumes/CodeSSD/PythonSwiftGithub/PySwiftWrapper")
-} else {
-    .package(url: "https://github.com/PythonSwiftLink/PySwiftWrapper", from: .init(0, 0, 0))
-}
-
 let package = Package(
     name: "KivyCore",
     platforms: [.iOS(.v13)],
@@ -38,13 +9,10 @@ let package = Package(
         .library(name: "KivyCore", targets: ["KivyCore"])
     ],
     dependencies: [
-        //.package(url: "https://github.com/KivySwiftLink/SDL2Core", .upToNextMajor(from: "311.0.0")),
-        //.package(url: "https://github.com/KivySwiftLink/PySwiftKit", .upToNextMajor(from: "311.0.0")),
-        pykit_package,
-        sdl_package,
-        psw,
+        .package(url: "https://github.com/KivySwiftLink/SDL2Core", .upToNextMajor(from: "311.0.0")),
+        .package(url: "https://github.com/KivySwiftLink/PySwiftKit", .upToNextMajor(from: "311.0.0")),
         .package(url: "https://github.com/KivySwiftLink/PythonCore", .upToNextMajor(from: "311.0.0")),
-        //.package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", .upToNextMajor(from: "0.0.0"))
+        .package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", .upToNextMajor(from: "0.0.0"))
     ],
     targets: [
         .target(
@@ -53,8 +21,7 @@ let package = Package(
         		.product(name: "SDL2Core", package: "SDL2Core"),
         		.product(name: "SwiftonizeModules", package: "PySwiftKit"),
         		.product(name: "PythonCore", package: "PythonCore"),
-        		"libkivy",
-                .product(name: "PySwiftWrapper", package: "PySwiftWrapper")
+        		"libkivy"
         	],
             resources: [
         	],
@@ -65,13 +32,12 @@ let package = Package(
         		.linkedFramework("CoreVideo")
         	],
             plugins: [
-        		//.plugin(name: "Swiftonize", package: "SwiftonizePlugin")
-            ]
+        		.plugin(name: "Swiftonize", package: "SwiftonizePlugin")]
         ),
         .binaryTarget(
             name: "libkivy",
-            url: "https://github.com/kivyswiftlink/KivyCore/releases/download/311.1.1/libkivy.zip",
-            checksum: "49447aa23ad6eedf8bd59f777041d8305c64d304aa6ae33cf7862ca1580eefff"
+            url: "https://github.com/kivyswiftlink/KivyCore/releases/download/311.1.2/libkivy.zip",
+            checksum: "eae6a0bf576319ea1f9f4f584a1ba18e26cec00f727d2afa7dfa7a78d0dacc7c"
         )
     ]
 )
